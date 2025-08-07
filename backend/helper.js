863e44e1bd7fda0db73e50bfaff665d5b22b86da
u3527338 <u3527338@gmail.com>
2025-08-07 15:09:58 +0800

    test fetch wordle.json

--- backend/helper.js
+++ backend/helper.js
@@ -1,6 +1,8 @@
 import GameHistoryModel from "./db/gameHistoryModel.js";
 import PlayerModel from "./db/playerModel.js";
-import wordleData from "./wordle.json" assert { type: "json" };
+import { readFileSync } from "fs";
+
+const wordleData = JSON.parse(readFileSync("./wordle.json", "utf-8"));
 
 export const findRoomIdByPlayerId = (rooms, id) => {
     for (const roomId in rooms) {
