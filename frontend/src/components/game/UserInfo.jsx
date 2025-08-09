60b66a48caef9e93677489af6f2ed1c93a9d1865
u3527338 <u3527338@gmail.com>
2025-08-09 16:27:27 +0800

    fix user info height

--- frontend/src/components/game/UserInfo.jsx
+++ frontend/src/components/game/UserInfo.jsx
@@ -6,7 +6,7 @@ const UserInfo = ({ player, flexDirection }) => (
         className={`user-info ${player?.isSelf ? "self" : "opponent"}`}
         sx={{
             display: "flex",
-            height: "-webkit-fill-available",
+            height: "auto",
             width:
                 flexDirection === "column" ? "20%" : "-webkit-fill-available",
             flexDirection,
@@ -19,7 +19,11 @@ const UserInfo = ({ player, flexDirection }) => (
         <Avatar
             src={avatar}
             alt={player?.name}
-            sx={{ width: { xs: 40, md: 60 }, height: { xs: 40, md: 60 }, backgroundColor: "#000" }}
+            sx={{
+                width: { xs: 40, md: 60 },
+                height: { xs: 40, md: 60 },
+                backgroundColor: "#000",
+            }}
         />
         <Typography variant="subtitle1" sx={{ fontSize: { xs: 12, sm: 16 } }}>
             {player?.name}
